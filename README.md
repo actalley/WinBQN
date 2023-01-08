6679d3f4c191e37e01f4b77f31f9ad0953c375d0
actalley <55100782+actalley@users.noreply.github.com>
2023-01-08 15:42:30 -0600

    Upstream input improvements and cbqn.dll

--- README.md
+++ README.md
@@ -4,14 +4,17 @@ WinBQN is a work in progress attempt at providing an easy to install and configu
 
 | Download                                 | Description     |
 | -----------------------------------------|-----------------|
-| [dzaima-cbqn-dev-7b7c31e-llvm-mingw-x86_64.zip](https://github.com/actalley/WinBQN/releases/download/v0.0.9-alpha/dzaima-cbqn-dev-7b7c31e-llvm-mingw-x86_64.zip) | Native CBQN built with [llvm-mingw](https://github.com/mstorsjo/llvm-mingw) from [dzaima/cbqn:develop 7b7c31e](https://github.com/dzaima/CBQN/tree/7b7c31e) |
+| [dzaima-cbqn-dev-381460e-llvm-mingw-x86_64.zip](https://github.com/actalley/WinBQN/releases/download/v0.0.10-alpha/dzaima-cbqn-dev-381460e-llvm-mingw-x86_64.zip) | Native CBQN built with [llvm-mingw](https://github.com/mstorsjo/llvm-mingw) from [dzaima/cbqn:develop 381460e](https://github.com/dzaima/CBQN/tree/381460e) |
+| [dll-dzaima-cbqn-dev-381460e-llvm-mingw-x86_64.zip](https://github.com/actalley/WinBQN/releases/download/v0.0.10-alpha/dll-dzaima-cbqn-dev-381460e-llvm-mingw-x86_64.zip) | Native `cbqn.dll` built with [llvm-mingw](https://github.com/mstorsjo/llvm-mingw) from [dzaima/cbqn:develop 381460e](https://github.com/dzaima/CBQN/tree/381460e) |
+| [dllstatic-dzaima-cbqn-dev-381460e-llvm-mingw-x86_64.zip](https://github.com/actalley/WinBQN/releases/download/v0.0.10-alpha/dllstatic-dzaima-cbqn-dev-381460e-llvm-mingw-x86_64.zip) | Native static linked `cbqn.dll` built with [llvm-mingw](https://github.com/mstorsjo/llvm-mingw) from [dzaima/cbqn:develop 381460e](https://github.com/dzaima/CBQN/tree/381460e) |
 
-Native builds currently error on repl input. It is possible to run `.bqn` files via `-f`, however they must have Unix line endings (for now). `UTF-8` console output has not been addressed. No `•FFI`, `•SH`, or `cbqn.dll` yet. Early days.
+Native builds currently do not handle double-struck character input correctly. It is possible to run `.bqn` files via `-f`, however they must have Unix line endings (for now). `UTF-8` console output has not been addressed. No `•FFI` or `•SH` yet. Early days.
 
 Old MSYS2 and Cygwin builds are still available in [Releases](https://github.com/actalley/WinBQN/releases), but there will be no new versions.
 
 ## Short(ish) Term Goals
 - [x] Produce native builds with no Cygwin/MSYS2 dependencies
-- [ ] Provide native `cbqn.dll` and `•FFI`
+- [x] Provide native `cbqn.dll`
+- [ ] Provide `•FFI`
 - [ ] Provide `•SH`
 - [ ] Provide usable repl without mintty
--- psakefile.ps1
+++ psakefile.ps1
@@ -15,17 +15,97 @@ BuildSetup {
 
 Task default -depends Dist
 
-Task Dist `
-    -depends CheckEnvironment, Build `
-    -description 'Builds a WinBQN distributon' `
+Task Dist -depends DistDzaimaCBQNDevDll {
+    
+    # if Dist is successful write dist\success file
+    New-Item -Path $distSuccessPath -ItemType File -Force | Out-Null
+}
+
+Task DistDzaimaCBQNDevDll `
+    -depends DistDzaimaCBQNDevStaticDll `
+    -description 'Builds a WinBQN dll distributon from dzaima/CBQN' `
 {
-    $rev = & git.exe submodule status |
-        Select-String -Pattern '^(-|\s)([\d\w]{7})[\d\w]+\sbuild/dzaima-CBQN-dev\s.+$' |
-        ForEach-Object { $_.Matches.Groups[2].Value }
 
-    Assert ( $rev ) 'Unable to retrieve submodule revision'
+    $distName = "dll-dzaima-cbqn-dev-$($script:dzaimaCBQNDevRev)-llvm-mingw-x86_64"
 
-    $distName = "dzaima-cbqn-dev-$rev-llvm-mingw-x86_64"
+    New-Item -Path dist\$distName -ItemType Directory | Out-Null
+
+    $libwinpthread = "${env:llvm-mingw}\x86_64-w64-mingw32\bin\libwinpthread-1.dll"
+
+    Assert ( Test-Path -Path $libwinpthread ) "`"$libwinpthread`" does not exist!"
+
+    $distItems = @{
+        $libwinpthread                                 = "dist\$distName\libwinpthread-1.dll"
+        'build\dzaima-CBQN-dev\cbqn.dll'               = "dist\$distName\cbqn.dll"
+        'build\dzaima-CBQN-dev\licenses\LICENSE-GPLv3' = "dist\$distName\licenses\CBQN_LICENSE"
+        "${env:llvm-mingw}\LICENSE.TXT"                = "dist\$distName\licenses\LLVM-MINGW_LICENSE"
+    }
+
+    $distItems.GetEnumerator() | ForEach-Object {
+
+        $destFolder = $_.Value | Split-Path -Parent
+
+        if ( -not (Test-Path -Path $destFolder)  ) {
+
+            New-Item -Path $destFolder -ItemType Directory -Force | Out-Null
+        }
+
+        Copy-Item -Path $_.Name -Destination $_.Value -Force 
+    }
+
+    $distItems.GetEnumerator() | ForEach-Object {
+
+        Assert ( Test-Path -Path $_.Value ) "`"$($_.Value)`" does not exist!"
+    }
+
+    Compress-Archive -Path "dist\$distName" -DestinationPath "dist\$distName.zip" -CompressionLevel Optimal
+
+    Assert ( Test-Path -Path "dist\$distName.zip" ) "`"dist\$distName.zip`" does not exist!"
+}
+
+Task DistDzaimaCBQNDevStaticDll `
+    -depends DistDzaimaCBQNDev `
+    -description 'Builds a WinBQN static linked dll distributon from dzaima/CBQN' `
+{
+
+    $distName = "dllstatic-dzaima-cbqn-dev-$($script:dzaimaCBQNDevRev)-llvm-mingw-x86_64"
+
+    New-Item -Path dist\$distName -ItemType Directory | Out-Null
+
+    $distItems = @{
+        'build\dzaima-CBQN-dev\cbqns.dll'               = "dist\$distName\cbqn.dll"
+        'build\dzaima-CBQN-dev\licenses\LICENSE-GPLv3' = "dist\$distName\licenses\CBQN_LICENSE"
+        "${env:llvm-mingw}\LICENSE.TXT"                = "dist\$distName\licenses\LLVM-MINGW_LICENSE"
+    }
+
+    $distItems.GetEnumerator() | ForEach-Object {
+
+        $destFolder = $_.Value | Split-Path -Parent
+
+        if ( -not (Test-Path -Path $destFolder)  ) {
+
+            New-Item -Path $destFolder -ItemType Directory -Force | Out-Null
+        }
+
+        Copy-Item -Path $_.Name -Destination $_.Value -Force 
+    }
+
+    $distItems.GetEnumerator() | ForEach-Object {
+
+        Assert ( Test-Path -Path $_.Value ) "`"$($_.Value)`" does not exist!"
+    }
+
+    Compress-Archive -Path "dist\$distName" -DestinationPath "dist\$distName.zip" -CompressionLevel Optimal
+
+    Assert ( Test-Path -Path "dist\$distName.zip" ) "`"dist\$distName.zip`" does not exist!" 
+}
+
+Task DistDzaimaCBQNDev `
+    -depends CheckEnvironment, BuildDzaimaCBQNDev, GetSubmoduleStatus `
+    -description 'Builds a WinBQN distributon from dzaima/CBQN' `
+{
+
+    $distName = "dzaima-cbqn-dev-$($script:dzaimaCBQNDevRev)-llvm-mingw-x86_64"
 
     New-Item -Path dist\$distName -ItemType Directory | Out-Null
 
@@ -36,6 +116,7 @@ Task Dist `
     $distItems = @{
         $libwinpthread                                 = "dist\$distName\libwinpthread-1.dll"
         'build\dzaima-CBQN-dev\BQN.exe'                = "dist\$distName\BQN.exe"
+        'build\dzaima-CBQN-dev\cbqn.dll'               = "dist\$distName\cbqn.dll"
         'build\dzaima-CBQN-dev\licenses\LICENSE-GPLv3' = "dist\$distName\licenses\CBQN_LICENSE"
         "${env:llvm-mingw}\LICENSE.TXT"                = "dist\$distName\licenses\LLVM-MINGW_LICENSE"
     }
@@ -60,9 +141,6 @@ Task Dist `
     Compress-Archive -Path "dist\$distName" -DestinationPath "dist\$distName.zip" -CompressionLevel Optimal
 
     Assert ( Test-Path -Path "dist\$distName.zip" ) "`"dist\$distName.zip`" does not exist!"
-
-    # if Dist is successful write dist\success file
-    New-Item -Path $distSuccessPath -ItemType File -Force | Out-Null
 }
 
 Task SandboxDist `
@@ -76,7 +154,7 @@ Task SandboxDist `
     Assert ( Test-Path -Path $distSuccessPath ) "`"$distSuccessPath`" does not exist, Dist must have failed in the sandbox!"
 }
 
-Task Build -depends GetCBQN {
+Task BuildDzaimaCBQNDev -depends GetDzaimaCBQNDev {
 
     # This is not necessarily how we want to build long term, this is just to get to a failing build so issues can be worked
 
@@ -92,12 +170,23 @@ Task Build -depends GetCBQN {
         & clang.exe -std=gnu11 -Wall -Wno-unused-function -fms-extensions -ffp-contract=off -fno-math-errno -Wno-microsoft-anon-tag -Wno-bitwise-instead-of-logical -Wno-unknown-warning-option -DBYTECODE_DIR=bytecodeSubmodule -DSINGELI=0 -DFFI=0 -fvisibility=hidden -DCBQN_EXPORT -DNO_MMAP -O3 -o BQN.exe src/opt/single.c -lm -lpthread
     }
 
+
+    exec {
+
+        & clang.exe -std=gnu11 -Wall -Wno-unused-function -fms-extensions -ffp-contract=off -fno-math-errno -Wno-microsoft-anon-tag -Wno-bitwise-instead-of-logical -Wno-unknown-warning-option -DBYTECODE_DIR=bytecodeSubmodule -DCBQN_SHARED -DSINGELI=0 -DFFI=0 -fvisibility=hidden -DCBQN_EXPORT -DNO_MMAP -O3 -o cbqn.dll src/opt/single.c -lm -lpthread -shared
+    }
+
+    exec {
+
+        & clang.exe -std=gnu11 -Wall -Wno-unused-function -fms-extensions -ffp-contract=off -fno-math-errno -Wno-microsoft-anon-tag -Wno-bitwise-instead-of-logical -Wno-unknown-warning-option -DBYTECODE_DIR=bytecodeSubmodule -DCBQN_SHARED -DSINGELI=0 -DFFI=0 -fvisibility=hidden -DCBQN_EXPORT -DNO_MMAP -O3 -o cbqns.dll src/opt/single.c -lm -lpthread -shared -static
+    }
+
     Pop-Location
 
     Assert ( Test-Path -Path 'build\dzaima-CBQN-dev\BQN.exe' ) 'BQN.exe does not exist!'
 }
 
-Task GetCBQN {
+Task GetDzaimaCBQNDev {
 
     exec {
 
@@ -105,6 +194,15 @@ Task GetCBQN {
     }
 
     Assert ( Test-Path -Path 'build\dzaima-CBQN-dev\makefile' ) 'CBQN makefile does not exist!'
+}
+
+Task GetSubmoduleStatus {
+
+    $submoduleStatus = & git.exe submodule status
+
+    $script:dzaimaCBQNDevRev = $submoduleStatus |
+        Select-String -Pattern '^(-|\s)([\d\w]{7})[\d\w]+\sbuild/dzaima-CBQN-dev' |
+        ForEach-Object { $_.Matches.Groups[2].Value }
 }
 
 Task CheckEnvironment -depends CheckClang, CheckGit
